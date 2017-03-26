5190cc5fe8f87ac9a9fe54af54cac1852dd31d83
kokikono <kk.k.0421@outlook.jp>
2017-03-26 16:54:40 +0900

    material fab menuの追加、だいぶ出来たが今はしっくり来ないので使わない

---
+++ app/src/main/java/local/koki/android/eventory/view/parts/FabSheetItemText.java
@@ -0,0 +1,17 @@
+package local.koki.android.eventory.view.parts;
+
+import android.content.Context;
+import android.widget.TextView;
+
+import local.koki.android.eventory.R;
+
+/**
+ * Created by kokikono on 2017/03/21.
+ */
+
+public class FabSheetItemText extends TextView{
+    public FabSheetItemText(Context context){
+        super(context);
+        //super(context,null, R.attr.TextAppearance_MaterialSheetFab_Sheet_Item);
+    }
+}
